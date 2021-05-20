40b57dbb4a6ee76a51fa910e0132f2b052ca2ea2
Mervin Villaceran <mervinvillaceran@gmail.com>
2021-05-21 07:52:31 +0800

    dashbaord sidebar and header

---
+++ src/components/NavGroup.vue
@@ -0,0 +1,37 @@
+<template>
+  <q-list>
+    <q-item-label
+        header
+        class="text-grey-8"
+    >
+        {{ header }}
+    </q-item-label>
+    <NavLink
+        v-for="link in links"
+        :key="link.title"
+        v-bind="link"
+    />
+  </q-list>
+</template>
+
+<script>
+import NavLink from './NavLink.vue'
+
+export default {
+  name: 'NavGroup',
+  props: {
+    header: {
+      type: String,
+      required: true
+    },
+
+    links: {
+      type: [],
+      default: []
+    },
+  },
+  components: {
+      NavLink
+  }
+}
+</script>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,101 @@
+<template>
+  <q-layout view="lHh Lpr lFf" class="layout-header">
+    <q-header elevated class="header text-black">
+      <q-toolbar class="toolbar">
+        <q-toolbar-title>
+          Quasar App
+        </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      class="drawer"
+      content-class="bg-white"
+    >
+      <q-list>
+        <q-item-label
+          header
+          class="text-grey-8"
+        >
+          <q-img
+            src="~assets/logo.png"
+            spinner-color="white"
+            class="logo"
+            />
+        </q-item-label>
+        <NavGroup
+            header="Main"
+          :links="essentialLinks"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import NavGroup from 'src/components/NavGroup.vue'
+
+const linksData = [
+  {
+    title: 'Docs',
+    caption: 'quasar.dev',
+    icon: 'school',
+    link: 'https://quasar.dev'
+  },
+  {
+    title: 'Github',
+    caption: 'github.com/quasarframework',
+    icon: 'code',
+    link: 'https://github.com/quasarframework'
+  },
+  {
+    title: 'Discord Chat Channel',
+    caption: 'chat.quasar.dev',
+    icon: 'chat',
+    link: 'https://chat.quasar.dev'
+  },
+  {
+    title: 'Forum',
+    caption: 'forum.quasar.dev',
+    icon: 'record_voice_over',
+    link: 'https://forum.quasar.dev'
+  },
+  {
+    title: 'Twitter',
+    caption: '@quasarframework',
+    icon: 'rss_feed',
+    link: 'https://twitter.quasar.dev'
+  },
+  {
+    title: 'Facebook',
+    caption: '@QuasarFramework',
+    icon: 'public',
+    link: 'https://facebook.quasar.dev'
+  },
+  {
+    title: 'Quasar Awesome',
+    caption: 'Community Quasar projects',
+    icon: 'favorite',
+    link: 'https://awesome.quasar.dev'
+  }
+];
+
+export default {
+  name: 'MainLayout',
+  components: { NavGroup },
+  data () {
+    return {
+      leftDrawerOpen: false,
+      essentialLinks: linksData
+    }
+  }
+}
+</script>
